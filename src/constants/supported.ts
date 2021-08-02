a0ed9f3d1bd2fa33b4db8d226ea3e62234fb3c13
Rahul Kothari <rahul.kothari.201@gmail.com>
2021-08-02 11:57:30 +0100

    upgrade web3modal version, add optimism, arbitrum

--- src/constants/supported.ts
+++ src/constants/supported.ts
@@ -29,11 +29,35 @@ export const SUPPORTED_CHAINS: ISupportedChains = {
     assets: ["eth", "dai"],
     blockExplorerUrl: "https://rinkeby.etherscan.io",
   },
+  10: {
+    name: "Optimism Mainnet",
+    chainId: 10,
+    assets: ["eth", "dai"],
+    blockExplorerUrl: "https://optimistic.etherscan.io",
+  },
+  69: {
+    name: "Optimism Kovan Testnet",
+    chainId: 69,
+    assets: ["eth", "dai"],
+    blockExplorerUrl: "https://kovan-optimistic.etherscan.io",
+  },
   100: {
     name: "xDAI",
     chainId: 100,
     assets: ["xdai"],
     blockExplorerUrl: "https://blockscout.com/poa/dai",
+  },
+  42161: {
+    name: "Arbitrum Mainnet",
+    chainId: 42161,
+    assets: ["eth", "dai"],
+    blockExplorerUrl: "https://explorer.arbitrum.io",
+  },
+  421611: {
+    name: "Arbitrum Rinkeby Testnet",
+    chainId: 421611,
+    assets: ["eth", "dai"],
+    blockExplorerUrl: "https://rinkeby-explorer.arbitrum.io",
   },
 };
 
@@ -86,6 +110,34 @@ export const SUPPORTED_ASSETS: ISupportedAssets = {
       contractAddress: "0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735",
     },
   },
+  10: {
+    eth: {
+      symbol: "ETH",
+      name: "Ethereum",
+      decimals: "18",
+      contractAddress: "",
+    },
+    dai: {
+      symbol: "DAI",
+      name: "DAI Stablecoin",
+      decimals: "18",
+      contractAddress: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+    },
+  },
+  69: {
+    eth: {
+      symbol: "ETH",
+      name: "Ethereum",
+      decimals: "18",
+      contractAddress: "",
+    },
+    dai: {
+      symbol: "DAI",
+      name: "DAI Stablecoin",
+      decimals: "18",
+      contractAddress: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
+    },
+  },
   100: {
     xdai: {
       symbol: "xDAI",
@@ -93,5 +145,33 @@ export const SUPPORTED_ASSETS: ISupportedAssets = {
       decimals: "18",
       contractAddress: "",
     },
+  },
+  421611: {
+    eth: {
+      symbol: "ETH",
+      name: "Ethereum",
+      decimals: "18",
+      contractAddress: ""
+    },
+  //   dai: {
+  //     symbol: "DAI",
+  //     name: "DAI Stablecoin",
+  //     decimals: "18",
+  //     contractAddress: "TODO",
+  //   },
+  },
+  42161: {
+    eth: {
+      symbol: "ETH",
+      name: "Ethereum",
+      decimals: "18",
+      contractAddress: "",
+    },
+  //   dai: {
+  //     symbol: "DAI",
+  //     name: "DAI Stablecoin",
+  //     decimals: "18",
+  //     contractAddress: "TODO",
+  //   },
   },
 };
